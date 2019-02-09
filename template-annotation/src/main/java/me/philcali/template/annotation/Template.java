5d9e8e88b9cc52e4bcd2e2c0f0b7360e63db8226
Philip Cali <philip.cali@gmail.com>
2019-02-09 20:45:56 +0000

    Moving the template annotation for meta programming cases.

---
+++ template-annotation/src/main/java/me/philcali/template/annotation/Template.java
@@ -0,0 +1,18 @@
+package me.philcali.template.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Template {
+    public static final String DEFAULT_ENGINE = "handlebars";
+
+    public static final String DEFAULT_LOCATION = "/templates";
+
+    String value() default DEFAULT_ENGINE;
+
+    String location() default DEFAULT_LOCATION;
+}
--- template-api/src/main/java/me/philcali/template/api/TemplateEngineProvider.java
+++ template-api/src/main/java/me/philcali/template/api/TemplateEngineProvider.java
@@ -1,7 +1,21 @@
 package me.philcali.template.api;
 
+import java.util.Optional;
+
+import me.philcali.template.annotation.Template;
 import me.philcali.template.api.exception.TemplateNotFoundException;
 
 public interface TemplateEngineProvider {
     TemplateEngine get(String templateId) throws TemplateNotFoundException;
+
+    default TemplateEngine get(final Template template) {
+        return Optional.ofNullable(template).map(t -> {
+            final TemplateEngine engine = new TemplateEngineComposite(get(t.value()));
+            engine.templatePrefix(t.location());
+            engine.templatePrefix(Template.DEFAULT_LOCATION);
+            return engine;
+        }).orElseGet(() -> {
+            return get(Template.DEFAULT_ENGINE);
+        });
+    }
 }
